87085c8d5478e5bc916fa94b2a167a2c00102bc6
xkwork <94737200+xkwork@users.noreply.github.com>
2023-04-14 17:04:48 +0800

    Create run.py

---
+++ run.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import streamlit as st
+
+st.title("商品数量汇总")
+
+uploaded_file = st.file_uploader("选择要上传的 Excel 文档", type=["xlsx"])
+
+if uploaded_file is not None:
+    df = pd.read_excel(uploaded_file)
+    grouped = df.groupby(["提货点/配送门店"]).sum()
+    st.write(grouped)
