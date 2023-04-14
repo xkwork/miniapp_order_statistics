5fada50161861b57c509b349578272176db1c66a
xkwork <94737200+xkwork@users.noreply.github.com>
2023-04-14 17:31:06 +0800

    Update run.py

--- run.py
+++ run.py
@@ -7,5 +7,5 @@ uploaded_file = st.file_uploader("选择要上传的 Excel 文档", type=["xlsx"
 
 if uploaded_file is not None:
     df = pd.read_excel(uploaded_file)
-    grouped = df.groupby(["提货点配送门店"]).sum()
+    grouped = df.groupby(["提货点/配送门店", "商品名称"]).size().reset_index(name='数量')
     st.write(grouped)
